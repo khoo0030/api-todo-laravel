87c58a21d8eeb884dd5d3e68bd9ba5311c489880
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2018-11-04 09:18:11 +0800

    Make Authenticated middleware throw exception when unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Authenticate extends Middleware
 {
@@ -14,6 +15,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        return route('login');
+        throw new UnauthorizedHttpException('', 'Unauthenticated');
     }
 }
