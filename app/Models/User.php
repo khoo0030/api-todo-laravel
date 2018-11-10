2aa2027bdbae53f85844069011e570a72ce9aa5c
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2018-11-10 15:27:58 +0800

    Add docblock

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
